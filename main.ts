83a83d9f4da170008b4c8dc614eedc24ca87795e
islahunterr <isla.k.hunter@student.uts.edu.au>
2024-11-27 13:49:50 +1100

    Adjusted sprite functions

--- main.ts
+++ main.ts
@@ -1,63 +1,36 @@
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
-    bee.destroy(effects.spray, 100)
-    music.baDing.play()
-    info.changeScoreBy(1)
-})
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
-    bee.destroy(effects.disintegrate, 100)
-    music.bigCrash.play()
+// When hero doesn't destroy a plastic bag - they lose 1 life.
+scene.onHitWall(SpriteKind.Enemy, function (sprite, location) {
+    sprites.destroy(otherSprite)
     info.changeLifeBy(-1)
 })
+// Aim: when player clashes into plastic bag it is supposed to disintegrate. Player should earn a point for destroying the pollution.
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite, effects.disintegrate, 100)
+    info.changeScoreBy(1)
+    // Gaming sound effect to enhance user - experience and replicate typical gaming experience
+    music.baDing.play()
+})
 let clover: Sprite = null
-let bee: Sprite = null
+let otherSprite: Sprite = null
 scene.setBackgroundImage(assets.image`background`)
 let hero = sprites.create(assets.image`hero`, SpriteKind.Player)
-controller.moveSprite(hero)
+controller.moveSprite(hero, 150, 0)
+// Set hero position to set on grass floor.
+hero.setPosition(35, 86)
+// Hero can't leave screen with user controls
 hero.setStayInScreen(true)
-game.onUpdateInterval(5000, function () {
-    clover = sprites.createProjectileFromSide(img`
-        ........................
-        ...........66...........
-        ..........6776..........
-        ..........6776..........
-        .........877778.........
-        ........86777768........
-        .......6777777776.......
-        ......677677776776......
-        ......866777777668......
-        .....86677677677668.....
-        ....8668866766888668....
-        ....8888668886686888....
-        .....86868868868668.....
-        ....866888668888868.....
-        ....8688886888888888....
-        ....8886688888866888....
-        ....8676888868886768....
-        ...87778868678688776....
-        ..8777767767787767778...
-        .877767777777677776778..
-        .8866777777777777776778.
-        .8667776776767776777688.
-        ..887766768668776667668.
-        ..8688668886688686688668
-        .86688688686866888688888
-        8668868866888866888868..
-        88886686688888868688668.
-        .8688888888888888668868.
-        .8878888868868878868788.
-        .87768776788778777667788
-        877677767787776767776778
-        88877787766777777877788.
-        ..88886786777667768888..
-        .....86887786668868.....
-        ......8886888668888.....
-        .........88ee88.........
-        .........feeeef.........
-        .........feeeef.........
-        ........feeefeef........
-        ........fefeffef........
-        `, randint(-50, 50), 50)
-    bee = sprites.createProjectileFromSide(img`
+info.setScore(0)
+// Character starts with 5 lives - lives diminish if they don't destroy all plastic bags.
+info.setLife(5)
+// Tree function - Intended to be kept alive - Don't let character destroy tree.
+game.onUpdateInterval(1000, function () {
+    clover = sprites.create(assets.image`tree`, SpriteKind.Food)
+    clover.setPosition(randint(10, 145), 8)
+    clover.setVelocity(0, 50)
+})
+// Plastic Bag Sprite - Intended to be destroyed.
+game.onUpdateInterval(1000, function () {
+    otherSprite = sprites.create(img`
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         . . 6 6 . . . . . . . . 6 6 . . 
@@ -74,6 +47,10 @@ game.onUpdateInterval(5000, function () {
         . . . 6 6 6 6 6 6 6 6 6 6 . . . 
         . . . . 6 6 6 6 6 6 6 6 . . . . 
         . . . . . . . . . . . . . . . . 
-        `, 50, 50)
-    bee.setKind(SpriteKind.Enemy)
+        `, SpriteKind.Enemy)
+    // Plastic bag and tree set to fall from the same place.
+    // Duplicated velocity and axis for game functionality.
+    // 
+    otherSprite.setPosition(randint(10, 145), 8)
+    otherSprite.setVelocity(0, 50)
 })
